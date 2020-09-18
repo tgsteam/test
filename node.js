a77620094c42d59fed7e9b218da5006915799880
HXY <2779799659@qq.com>
2020-09-18 20:48:04 +0800

    使用nodejs express模块

--- node.js
+++ node.js
@@ -1,26 +1,20 @@
-var express = require('express')
-var fs = require('fs')
-var path = require('path')
-var http = require('http')
-var https = require('https')
-var ejs = require('ejs')
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
 var app = express();
-app.use(express.static(path.join(__dirname, '../SQG')));
-app.set('views', path.join(__dirname, '../SQG'));
-app.set('view engine', 'ejs');
+var path = require('path');
 
-var router = express.Router();
-router.get('/', function (req, res, next) {
-    res.render('index',
-        {
-            image: 'https://lablog-images.oss-cn-beijing.aliyuncs.com/images/lablog.gif',
+app.use(express.static(path.join(__dirname, './')));
+
+app.get('/', function(req, res, next) {
+    res.writeHead(200,{'Content-Type':'text/html'})
+    fs.readFile('index.html','utf-8',function(err,data){
+        if(err){
+        throw err ;
+        }
+        res.end(data);
         });
 });
-router.get('/ping', function (req, res, next) {
-    res.end('pong');
-});
-app.use('/', router);
-var httpServer = http.createServer(app);
-httpServer.listen(80);
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
+
+app.listen(4080);
+console.log("监听端口 4080");
